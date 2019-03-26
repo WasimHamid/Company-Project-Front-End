6cadcfd1d01972fe079f48457d05723ac646f4cd
Your Name <you@example.com>
2019-03-26 14:54:07 +0000

    loading page buttons, non functional

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import SearchField from "react-search-field";
+
+import "./App.css";
+
+const API = "http://localhost:5000";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sessionResponse: "",
+      query: ""
+    };
+  }
+
+  callSessionAPI() {
+    fetch(`${API}/session`)
+      .then(res => res.text())
+      .then(res => this.setState({ sessionResponse: res }));
+  }
+
+  handleSearchChange = () => {
+    fetch(`${API}/employees`)
+      .then(res => res.json())
+      .then(res =>
+        this.setState({
+          query: this.state.staffNumber
+        })
+      );
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => this.callSession}>Start New Session</button>
+          <SearchField
+            placeholder="Search by Employee ID"
+            onChange={this.handleSearchChange}
+            classNames="test-class"
+          />
         </header>
       </div>
     );
