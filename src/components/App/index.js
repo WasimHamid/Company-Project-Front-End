d49295b7a6ab5e7a1214f79bdc81eb4112ae1e6a
robynv <robyn.vann95@gmail.com>
2019-03-26 12:05:08 +0000

    Basic front end. Add employee button and dialog started

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,25 +1,107 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import TextField from "@material-ui/core/TextField";
+
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openDialog: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(() => ({ openDialog: true }));
+  };
+
+  handleClose = () => {
+    this.setState(() => ({ openDialog: false }));
+  };
+
+  // searchClick = () => {
+  //   fetch("http://localhost:5000/")
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(function(myJson) {
+  //       console.log(JSON.stringify(myJson));
+  //     });
+  // };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div>
+          <Button onClick={this.handleClick}>Add Employee</Button>
+          <Dialog
+            open={this.state.openDialog}
+            onClose={this.handleClose}
+            aria-labelledby="form-dialog-title"
           >
-            Learn React
-          </a>
-        </header>
+            <DialogTitle id="form-dialog-title">Employee Search</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Input the employee name and click search to find relevant
+                employees
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="search"
+                label="Search"
+                type="search"
+                fullWidth
+              />
+              <Button color="primary">Search</Button>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                Close
+              </Button>
+              <Button onClick={this.handleClose} color="primary">
+                Ok
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <div>Employee Name:</div>
+          <button>Save</button>
+          <button>View Edits</button>
+          <button>Previous Scores</button>
+          <button>Close</button>
+          <div>
+            <h2>Impact</h2>
+            <h2>Potential Category</h2>
+            <h2>Potential Score</h2>
+          </div>
+          <div>
+            <input type="radio" />
+            <input type="radio" />
+            <input type="radio" />
+            <input type="radio" />
+            <input type="radio" />
+          </div>
+          <div>
+            <input type="radio" />
+            <input type="radio" />
+            <input type="radio" />
+            <input type="radio" />
+            <input type="radio" />
+          </div>
+          <div>
+            <input type="radio" />
+            <input type="radio" />
+            <input type="radio" />
+            <input type="radio" />
+            <input type="radio" />
+          </div>
+        </div>
       </div>
     );
   }
